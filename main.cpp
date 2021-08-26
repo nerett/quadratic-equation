fc934f88c38a971c1f677839afd21d563343b7d2
nerett <pt-mvb@inbox.lv>
2021-08-26 16:16:16 +0300

    use pointers

--- main.cpp
+++ main.cpp
@@ -14,15 +14,14 @@ struct quadratic_equation {
 
 //struct quadratic_equation bea_input ( struct quadratic_equation );
 //void bea_output ( struct quadratic_equation );
-struct quadratic_equation solve_equation ( struct quadratic_equation );
-struct quadratic_equation check_solvability ( struct quadratic_equation );
-double calc_discriminant( struct quadratic_equation );
-struct quadratic_equation create_equation ( double, double, double );
+struct quadratic_equation solve_equation( struct quadratic_equation* );
+struct quadratic_equation check_solvability( struct quadratic_equation* );
+double calc_discriminant( struct quadratic_equation* );
+struct quadratic_equation set_equation_coeffs( struct quadratic_equation*, double, double, double );
 
 int main()
 {
-    double coeff_a = 1, coeff_b = 1, coeff_c = 1; // a*X^2+b*X+c=0
-    //struct
+    double coeff_a = NAN, coeff_b = NAN, coeff_c = NAN; // a*X^2+b*X+c=0
     quadratic_equation equation;
 
 
@@ -31,7 +30,7 @@ int main()
     printf( "a*X^2+b*X+c=0 \n" );
     printf( "Введите коэффициент a: \n" );
 
-    scanf( "%lf", &coeff_a );
+    scanf( "%lf", &coeff_a ); //проверка присваиванием
 
     printf( "Введите коэффициент b: \n" );
 
@@ -41,11 +40,11 @@ int main()
 
     scanf( "%lf", &coeff_c );
 
-    printf( "%s%f%s%f%s%f%s", "Введено уравнение ", coeff_a, "*X^2+", coeff_b, "*X+", coeff_c, "=0 \n \n" ); // адекватный вывод будет позже
+    printf( "Введено уравнение %fX^2+%f*X+%f=0 \n \n", coeff_a, coeff_b, coeff_c); // адекватный вывод будет позже
 
 
-    equation = create_equation( coeff_a, coeff_b, coeff_c );
-    equation = solve_equation( equation );
+    set_equation_coeffs( &equation, coeff_a, coeff_b, coeff_c );
+    solve_equation( &equation );
 
 
     if ( equation.is_solvable == YES )
@@ -76,45 +75,37 @@ int main()
 
 // quadratic_equation
 
-struct quadratic_equation create_equation ( double coeff_a, double coeff_b, double coeff_c )
+struct quadratic_equation set_equation_coeffs( struct quadratic_equation* temporary ,double coeff_a, double coeff_b, double coeff_c ) //написат ассерты
 {
-    struct quadratic_equation temporary;
-
-    temporary.coeff_a = coeff_a;
-    temporary.coeff_b = coeff_b;
-    temporary.coeff_c = coeff_c;
-
-    return temporary;
+    temporary->coeff_a = coeff_a;
+    temporary->coeff_b = coeff_b;
+    temporary->coeff_c = coeff_c;
 }
 
-double calc_discriminant ( struct quadratic_equation temporary )
+double calc_discriminant( struct quadratic_equation* temporary ) //написать сравнение с нулём нормально, fabs compare_with_zero
 {
-    double discriminant = (temporary.coeff_b * temporary.coeff_b) - 4*(temporary.coeff_a * temporary.coeff_c);
+    double discriminant = (temporary->coeff_b * temporary->coeff_b) - 4*(temporary->coeff_a * temporary->coeff_c);
     return discriminant;
 }
 
-struct quadratic_equation check_solvability ( struct quadratic_equation temporary )
+struct quadratic_equation check_solvability( struct quadratic_equation* temporary )
 {
     double discriminant = calc_discriminant( temporary );
 
     if ( discriminant >= 0 )
-        temporary.is_solvable = YES;
+        temporary->is_solvable = YES;
     else
-        temporary.is_solvable = NO;
-
-    return temporary;
+        temporary->is_solvable = NO;
 }
 
-struct quadratic_equation solve_equation ( struct quadratic_equation temporary )
+struct quadratic_equation solve_equation( struct quadratic_equation* temporary ) //частные случаи линейное квадратное
 {
     double discriminant = calc_discriminant( temporary );
-    temporary = check_solvability( temporary );
+    check_solvability( temporary );
 
-    if( temporary.is_solvable == YES )
+    if( temporary->is_solvable == YES )
     {
-        temporary.root_1 = ( -1* temporary.coeff_b + sqrt(discriminant) ) / ( 2* temporary.coeff_a );
-        temporary.root_2 = ( -1* temporary.coeff_b - sqrt(discriminant) ) / ( 2* temporary.coeff_a );
+        temporary->root_1 = ( -1* temporary->coeff_b + sqrt(discriminant) ) / ( 2* temporary->coeff_a );
+        temporary->root_2 = ( -1* temporary->coeff_b - sqrt(discriminant) ) / ( 2* temporary->coeff_a );
     }
-
-    return temporary;
 }
